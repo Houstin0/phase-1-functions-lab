81433d23821e84a7c11e7846c506c300dd3f1495
Houstin <houstin.angwenyi@student.moringaschool.com>
2023-06-14 07:09:30 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solution in this file!
+const hq = 42
+let blocks = 50
+
+function distanceFromHqInBlocks(blocks) {
+    const distance=Math.abs(blocks - hq)
+
+     return distance
+  }
+  function distanceFromHqInFeet(blocks) {
+    const blockLengthInFeet = 264
+
+    return distanceFromHqInBlocks(blocks) * blockLengthInFeet
+  }
+
+  function distanceTravelledInFeet(start, destination) {
+    const blockLengthInFeet = 264
+
+    const distanceInBlocks = Math.abs(destination - start) 
+    const distanceInFeet = distanceInBlocks * blockLengthInFeet
+
+    return distanceInFeet
+  }
+
+  function calculatesFarePrice(start, destination) {
+    const distanceInFeet = distanceTravelledInFeet(start, destination)
+    let farePrice
+  
+    if (distanceInFeet <=400) {
+      return farePrice = 0
+    } else if (distanceInFeet > 400 && distanceInFeet <= 2000) {
+      return farePrice = (distanceInFeet - 400) * 0.02
+    } else if (distanceInFeet > 2000 && distanceInFeet <= 2500) {
+      return farePrice = 25
+    } else if(distanceInFeet>2500) {
+      return farePrice = 'cannot travel that far'
+    }
+  }
